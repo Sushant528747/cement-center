13996e612ad968fcb85ab10d1f27206a7163fc48
Sushant <sushantmishra.kiit@gmail.com>
2018-07-28 22:23:02 +0530

    initial project setup

---
+++ cement-center-backend/src/main/java/com/cementcenter/management/CementCenterBackendApplication.java
@@ -0,0 +1,12 @@
+package com.cementcenter.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CementCenterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CementCenterBackendApplication.class, args);
+	}
+}
---
+++ cement-center-backend/src/test/java/com/cementcenter/management/CementCenterBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cementcenter.management;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CementCenterBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
